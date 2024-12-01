e849f549a012d613cef17238de80a2dff840e5cb
Søren Lausen <soelau01@iba.dk>
2024-12-01 16:31:11 +0100

    1

--- js/nav.js
+++
@@ -1,9 +0,0 @@
-const bars = document.querySelector("#bars");
-
-const openNav = () => {
-    document.querySelector("header > nav").classList.toggle("show");
-    bars.classList.toggle("fa-bars");
-    bars.classList.toggle("fa-xmark");
-}
-
-bars.addEventListener("click", openNav);
