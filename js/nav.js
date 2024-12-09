9d031246cde0bb036bfe9b31b05b1594a91e8410
Søren Lausen <soelau01@iba.dk>
2024-12-09 10:57:56 +0100

    2

--- js/nav.js
+++ js/nav.js
@@ -7,3 +7,22 @@ const openNav = () => {
 }
 
 bars.addEventListener("click", openNav);
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+  } else {
+     document.querySelector("header").style.top = "-80px";
+  }
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".breadtrail").style.top = "80px";
+  } else {
+     document.querySelector(".breadtrail").style.top = "-40px";
+  }
+
+  prevScrollpos = currentScrollPos;
+}
